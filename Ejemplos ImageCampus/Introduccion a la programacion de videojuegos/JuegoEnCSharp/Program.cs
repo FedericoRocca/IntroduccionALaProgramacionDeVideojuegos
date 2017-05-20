cddb400f69526cf5a15b7ec86b7dc9aeaf22b800
Federico Tomas Rocca <frocca17@gmail.com>
2017-05-20 12:48:25 -0300

    -Primera version de JuegoenCSharp 		- Primera version de movimiento de jugador

--- Ejemplos ImageCampus/Introduccion a la programacion de videojuegos/JuegoEnCSharp/Program.cs
+++ Ejemplos ImageCampus/Introduccion a la programacion de videojuegos/JuegoEnCSharp/Program.cs
@@ -17,6 +17,17 @@ namespace Clases
         public int getY() { return Y; }
         public void setX(int NuevoX) { X = NuevoX; }
         public void setY(int NuevoY) { Y = NuevoY; }
+        public void setImage(char NewImage) { Image = NewImage; }
+        public char getImage() { return Image; }
+        public void MoveLeft() { X--; }
+        public void MoveRigth() { X++; }
+        public void MoveDown() { Y++; }
+        public void MoveUp() { Y--; }
+        public void PrintChar()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(Image);
+        }
 
     }
 
@@ -26,8 +37,83 @@ namespace JuegoEnCSharp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            Clases.Personaje Jugador = new Clases.Personaje();
+            Clases.Personaje Enemigo = new Clases.Personaje();
+
+            Jugador.setX(1);
+            Jugador.setY(1);
+            Jugador.setImage('@');
+
+            ConsoleKeyInfo UserInput = new ConsoleKeyInfo();
+
+            bool Game = true;
+            while (Game)
+            {
+
+                if( Console.KeyAvailable )
+                {
+                    UserInput = Console.ReadKey();
+
+                    switch( UserInput.Key )
+                    {
+                        case ConsoleKey.UpArrow:
+                            {
+                                if( Jugador.getY() - 1 >= 1 )
+                                {
+                                    Jugador.MoveUp();
+                                }
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            {
+                                if (Jugador.getY() + 1 <= 49)
+                                {
+                                    Jugador.MoveDown();
+                                }
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            {
+                                if (Jugador.getX() - 1 >= 1)
+                                {
+                                    Jugador.MoveLeft();
+                                }
+                            }
+                            break;
+                        case ConsoleKey.RightArrow:
+                            {
+                                if (Jugador.getX() + 1 <= 98)
+                                {
+                                    Jugador.MoveRigth();
+                                }
+                            }
+                            break;
+                        case ConsoleKey.Escape:
+                            {
+                                Game = false;
+                                System.Console.Clear();
+                                System.Console.WriteLine("Nos vemos!");
+                            }
+                            break;
+                    }
+
+                }
+
+                if( Game == true )
+                {
+                    Jugador.PrintChar();
+                    System.Threading.Thread.Sleep(25);
+                    System.Console.Clear();
+                }
+                
+            }
+
         }
+
     }
+
 }
