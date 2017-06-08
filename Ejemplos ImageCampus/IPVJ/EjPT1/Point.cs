6afb433c7b2bc093b578b92363b107871793dde5
Federico Tomas Rocca <frocca17@gmail.com>
2017-06-07 21:49:21 -0300

    - Agregada parte 1 (Unfinished) del ejercicio uno de clases 	(7/6/2017)

---
+++ Ejemplos ImageCampus/IPVJ/EjPT1/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjPT1
+{
+    class Point
+    {
+
+        private int x;
+        private int y;
+
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Point(int NewX, int NewY)
+        {
+            x = NewX;
+            y = NewY;
+        }
+
+        public void setY(int NewY) { y = NewY; }
+        public void setX( int NewX ) { x = NewX; }
+        public int getX() { return x; }
+        public int gety() { return y; }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write( "X" );
+        }
+
+    }
+}
