762239bf9aff37f675fb008827c331fdc9a43ca0
Federico Tomas Rocca <frocca17@gmail.com>
2017-07-01 12:12:50 -0300

    - Agregado de ejemplos de pase de parametros por referencia

---
+++ Ejemplos ImageCampus/IPVJ/PasajePorReferencia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasajePorReferencia
+{
+    class Program
+    {
+
+        static void HacerAlgo(ref int FunctionParameter)
+        {
+            FunctionParameter = 10;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int MainParameter = 0;
+
+            HacerAlgo(ref MainParameter);
+
+            Console.WriteLine(MainParameter);
+            Console.Read();
+
+        }
+    }
+}
